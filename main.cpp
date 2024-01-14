cfcb7559fbb4df687f3b386086282eefc3b43de0
Distenz <18554509+distenz@users.noreply.github.com>
2024-01-14 18:24:15 +0100

    Update mesh with hidden faces removal via zbuffer

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "model.h"
 #include "tgaimage.h"
 #include <algorithm>
+#include <limits>
 
 #define ARTIFACT_NAME "artifact.tga"
 #define DEBUG true
@@ -158,7 +159,7 @@ void triangle(TGAImage &image, const TGAColor &color, int x0, int y0, int x1,
   }
 }
 
-Vec3f barycentric(const Vec2i *pts, const Vec2i P) {
+Vec3f barycentric(const Vec3f *pts, const Vec3f P) {
   Vec3f u = Vec3f(pts[2].x - pts[0].x, pts[1].x - pts[0].x, pts[0].x - P.x) ^
             Vec3f(pts[2].y - pts[0].y, pts[1].y - pts[0].y, pts[0].y - P.y);
   if (std::abs(u.z) < 1)
@@ -166,13 +167,16 @@ Vec3f barycentric(const Vec2i *pts, const Vec2i P) {
   return Vec3f(1. - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
 }
 
-void triangle2(TGAImage &image, const TGAColor &color, const Vec2i *pts) {
-  Vec2i boundingBoxMin(image.get_width() - 1, image.get_height() - 1),
-      boundingBoxMax(0, 0),
+void triangle2(TGAImage &image, float *zbuffer, const TGAColor &color,
+               const Vec3f *pts) {
+  Vec2f boundingBoxMin(std::numeric_limits<float>::max(),
+                       std::numeric_limits<float>::max()),
+      boundingBoxMax(-std::numeric_limits<float>::max(),
+                     -std::numeric_limits<float>::max()),
       imageBoundary(image.get_width() - 1, image.get_width() - 1);
   for (int i = 0; i < 3; i++) {
-    boundingBoxMin.x = std::max(0, std::min(boundingBoxMin.x, pts[i].x));
-    boundingBoxMin.y = std::max(0, std::min(boundingBoxMin.y, pts[i].y));
+    boundingBoxMin.x = std::max(0.f, std::min(boundingBoxMin.x, pts[i].x));
+    boundingBoxMin.y = std::max(0.f, std::min(boundingBoxMin.y, pts[i].y));
 
     boundingBoxMax.x =
         std::min(imageBoundary.x, std::max(boundingBoxMax.x, pts[i].x));
@@ -180,20 +184,34 @@ void triangle2(TGAImage &image, const TGAColor &color, const Vec2i *pts) {
         std::min(imageBoundary.y, std::max(boundingBoxMax.y, pts[i].y));
   }
 
-  Vec2i iter;
+  Vec3f iter;
   for (iter.x = boundingBoxMin.x; iter.x < boundingBoxMax.x; iter.x++) {
     for (iter.y = boundingBoxMin.y; iter.y < boundingBoxMax.y; iter.y++) {
       Vec3f barycentricP = barycentric(pts, iter);
       if (barycentricP.x < 0 || barycentricP.y < 0 || barycentricP.z < 0)
         continue;
-      image.set(iter.x, iter.y, color);
+      iter.z = 0;
+      for (int i = 0; i < 3; i++) {
+        iter.z += pts[i].z * barycentricP.z;
+      }
+      if (zbuffer[int(iter.x + iter.y * width)] < iter.z) {
+        zbuffer[int(iter.x + iter.y * width)] = iter.z;
+        image.set(iter.x, iter.y, color);
+      }
     }
   }
 }
-
+Vec3f world2screen(Vec3f v) {
+  return Vec3f(int((v.x + 1.) * width / 2. + .5),
+               int((v.y + 1.) * height / 2. + .5), v.z);
+}
 void mesh(Model *model, const TGAColor &color, TGAImage &image) {
 
   Vec3f light_dir(0, 0, -1);
+  float *zbuffer = new float[width * height];
+  for (int i = width * height; i--;
+       zbuffer[i] = -std::numeric_limits<float>::max())
+    ;
 
   int nFaces = model->nfaces();
   int hw = image.get_width() / 2, hh = image.get_height() / 2;
@@ -201,14 +219,14 @@ void mesh(Model *model, const TGAColor &color, TGAImage &image) {
   for (int i = 0; i < nFaces; i++) {
     std::vector<int> face = model->face(i);
 
+    Vec3f vertex;
     Vec3f world_coords[3];
-    Vec2i screen_coords[3];
+    Vec3f screen_coords[3];
 
     for (int j = 0; j < 3; j++) {
-      Vec3f vertex = model->vert(face[j]);
+      vertex = model->vert(face[j]);
       world_coords[j] = vertex;
-      screen_coords[j] =
-          Vec2i((world_coords[j].x + 1) * hw, (world_coords[j].y + 1) * hh);
+      screen_coords[j] = world2screen(vertex);
     }
 
     Vec3f normal = (world_coords[2] - world_coords[0]) ^
@@ -218,9 +236,23 @@ void mesh(Model *model, const TGAColor &color, TGAImage &image) {
 
     if (intensity > 0) {
       triangle2(
-          image,
+          image, zbuffer,
           TGAColor(intensity * 255, intensity * 255, intensity * 255, 255),
           screen_coords);
+    }
+  }
+}
+void rasterize(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color,
+               int ybuffer[]) {
+  if (p0.x > p1.x) {
+    std::swap(p0, p1);
+  }
+  for (int x = p0.x; x <= p1.x; x++) {
+    float t = (x - p0.x) / (float)(p1.x - p0.x);
+    int y = p0.y * (1. - t) + p1.y * t;
+    if (ybuffer[x] < y) {
+      ybuffer[x] = y;
+      image.set(x, 0, color);
     }
   }
 }
@@ -235,19 +267,43 @@ void exampleLines(TGAImage &image) {
 }
 
 void exampleRaster(TGAImage &image) {
-  Vec2i t0[3] = {Vec2i(10, 70), Vec2i(50, 160), Vec2i(70, 80)};
-  Vec2i t1[3] = {Vec2i(180, 50), Vec2i(150, 1), Vec2i(70, 180)};
-  Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)};
+  Vec3f t0[3] = {Vec3f(10, 70, 0), Vec3f(50, 160, 0), Vec3f(70, 80, 0)};
+  Vec3f t1[3] = {Vec3f(180, 5, 00), Vec3f(150, 1, 0), Vec3f(70, 180, 0)};
+  Vec3f t2[3] = {Vec3f(180, 1, 050), Vec3f(120, 160, 0), Vec3f(130, 180, 0)};
+  float *zb = new float[width * height];
 
-  triangle2(image, red, t0);
-  triangle2(image, white, t1);
-  triangle2(image, green, t2);
+  triangle2(image, zb, red, t0);
+  triangle2(image, zb, white, t1);
+  triangle2(image, zb, green, t2);
+}
+void exampleMesh(TGAImage &image) {
+  model = new Model{"./obj/head.obj"};
+  mesh(model, green, image);
+}
+void exampleYBuffer1(TGAImage &image) {
+  // scene "2d mesh"
+  line(image, red, 20, 34, 744, 400);
+  line(image, green, 120, 434, 444, 400);
+  line(image, blue, 330, 463, 594, 200);
+
+  // screen line
+  line(image, white, 10, 10, 790, 10);
+}
+void exampleYBuffer2(TGAImage &image) {
+  int ybuffer[width];
+  for (int i = 0; i < width; i++) {
+    ybuffer[i] = std::numeric_limits<int>::min();
+  }
+  rasterize(Vec2i(20, 34), Vec2i(744, 400), image, red, ybuffer);
+  rasterize(Vec2i(120, 434), Vec2i(444, 400), image, green, ybuffer);
+  rasterize(Vec2i(330, 463), Vec2i(594, 200), image, blue, ybuffer);
 }
 
 enum Examples {
   LINES = 0,
   RASTER = 1,
   MESH = 2,
+  YBUFFER = 3,
 };
 
 int main(int argc, char *argv[]) {
@@ -263,9 +319,13 @@ int main(int argc, char *argv[]) {
     exampleRaster(image);
     break;
   case MESH:
+    exampleMesh(image);
+    break;
+  case YBUFFER:
+    exampleYBuffer2(image);
+    break;
   default:
-    model = new Model{"./obj/head.obj"};
-    mesh(model, green, image);
+    exampleMesh(image);
   }
 
   image.flip_vertically();
